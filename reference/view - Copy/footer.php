99b495411e473ee5a7552557f4d0a8bcf13493af
ipakey <bornagainyfke@gmai.com>
2021-09-29 11:44:13 +0100

    remove all coding to start again

--- reference/view - Copy/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <?php if($number){ ?>
-        <a href="index.php">Click here to play again</a>
-
-<?php }
-    else{ ?>
-        🚀🚀🚀🚀🚀🚀
-  <?php  } ?>
-</footer>
--- view/header.php
+++
@@ -1,5 +0,0 @@
-<header class="header">
-    <h2>
-        <a href="index.php">Card Set <br class="title-break">Flash Cards</a>
-    </h2>
-</header>
